9e96f993c401e9dd099d9ea95083e9ca9dbcf64c
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2022-08-12 20:48:02 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -1,4 +1,4 @@
-var salario = 2000.00;
+var salario = 2000.50;
 var aumento = 0.15;
 var inflacao = 0.07;
 
@@ -7,3 +7,8 @@ novoSalarioInflacao = novoSalario - ( inflacao * novoSalario );
 
 console.log( novoSalario );
 console.log( novoSalarioInflacao );
+
+var strnovoSalario = `O novo salário é:\n R$\t ${novoSalario}`;
+var strnovoSalarioInflacao = `O novo salário aplicada inflação é:\n R$  ${novoSalarioInflacao}`;
+console.log( strnovoSalario );
+console.log( strnovoSalarioInflacao );
