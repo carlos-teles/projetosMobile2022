a3bad1fc96a085b1d318c46299f525582243c79f
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2022-09-02 20:10:12 -0300

    Update app.js

--- 013-projeto/app.js
+++ 013-projeto/app.js
@@ -1,1 +1,30 @@
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} value="Texto padrão" />
+      <TextInput style={styles.input} placeholder="Texto Substituido" />
+      <TextInput style={styles.input} secureTextEntry={true}  />
+      <TextInput style={styles.input} disabled={true}  />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },  
+});
