63f7be5703f542dbac3d597f604ff06aa8065865
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2022-09-30 20:31:21 -0300

    Add files via upload

--- projeto016/App.js
+++ projeto016/App.js
@@ -3,13 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const carrosJSON = [
-    { id: 1, marca: "Saab", anoFabrica:2017, combustivel: "Gasolina", preco: 5 },
-    { id: 2, marca: "Volvo", anoFabrica:2019, combustivel: "Gasolina", preco: 2 },
-    { id: 3, marca: "BMW", anoFabrica: 2012, combustivel: "Diesel" , preco: 3},
-    { id: 4, marca: "Vw", anoFabrica: 2020, combustivel: "Gasolina", preco: 3 },
-    { id: 5, marca: "Toyota",anoFabrica:2022, combustivel: "GNV", preco: 3 },
-    { id: 6, marca: "Audi",anoFabrica:2022, combustivel: "GNV", preco: 1 },
-    { id: 7, marca: "Audi TT",anoFabrica:2022, combustivel: "GNV", preco: 1 },    
+    { id: 1, marca: "Saab", anoFabrica:2017, combustivel: "Gasolina", preco: 50000 },
+    { id: 2, marca: "Volvo", anoFabrica:2019, combustivel: "Gasolina", preco: 200000 },
+    { id: 3, marca: "BMW", anoFabrica: 2012, combustivel: "Diesel" , preco: 300000},
+    { id: 4, marca: "Vw", anoFabrica: 2020, combustivel: "Gasolina", preco: 300000 },
+    { id: 5, marca: "Toyota",anoFabrica:2022, combustivel: "GNV", preco: 256000 },
+    { id: 6, marca: "Audi",anoFabrica:2022, combustivel: "GNV", preco: 1000000 },
+    { id: 7, marca: "Audi TT",anoFabrica:2022, combustivel: "GNV", preco: 1000000 },    
   ];
   //console.log(carrosJSON.find((carro) => carro.anoFabrica > 2019));
   //console.log(carrosJSON.some((carro) => carro.anoFabrica > 2019));
@@ -30,7 +30,7 @@ export default function App() {
 
 //console.log(carrosJSON.filter((carro) => carro.marca.includes("Audi")));
 
-console.log( carrosJSON.reduce((total, carro) => total + carro.preco, 0)); 
+//console.log( carrosJSON.reduce((total, carro) => total + carro.preco, 0)); 
 
   const carros = new Array("Saab", "Volvo", "BMW", "Vw" , "Toyota");
   const carrosIdade = new Array(5, 3, 10, 2 , 0);
@@ -48,9 +48,10 @@ console.log( carrosJSON.reduce((total, carro) => total + carro.preco, 0));
     <View style={styles.container}>
       <Text>---
         {"\n"}
-        {carrosJSON.map(({ marca, combustivel }) => (
-         `Marca do carro ${marca} combustível ${combustivel}.\n`
+        {carrosJSON.map(({ marca, combustivel, preco }) => (
+         `Marca do carro ${marca} combustível ${combustivel} preço R$ ${preco}\n`
         ))}
+        {`Valor Total:R$`+carrosJSON.reduce((total, carro) => total + carro.preco, 0)}
         {"\n"}
       ---</Text>
       <StatusBar style="auto" />
