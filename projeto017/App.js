65c34121a0f876cb30e45b300bd06a025bb33596
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2022-10-03 18:59:32 -0300

    Update App.js

--- projeto017/App.js
+++ projeto017/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState, Component } from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Provider, Appbar, Avatar, TextInput, HelperText } from 'react-native-paper';
+
 
 export default function App() {
   const [busca, setBusca] = useState('');
@@ -8,10 +10,16 @@ export default function App() {
   const [apresentaTxt, setApresentaTxt] = useState('');
   const [carregando, setCarregando] = useState(false);
 
+  const validateAll = () => {
+    console.log("Teste");
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* inicio campo busca */}
+      <Text style={styles.labelText}>Busca:</Text>
+      <TextInput style={styles.inputText} placeholder="Informe a busca" onChangeText={busca => setBusca(busca)} />{"\n"}
+      <Button title="Enviar os dados" style={styles.buttonstyle} onPress={() => {validateAll();}} color="#6200EE" />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonstyle: {
+    marginTop: 10,
+  },  
+  inputText: {
+    width: 200, height: 40, backgroundColor: 'powderblue'
+  },  
 });
