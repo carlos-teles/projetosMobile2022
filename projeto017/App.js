1daa00bead29294a43f1465b789c748f6bde5ad2
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2022-10-04 19:31:37 -0300

    Update App.js

--- projeto017/App.js
+++ projeto017/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState, Component } from "react";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
 import { Provider, Appbar, Avatar, TextInput, HelperText } from 'react-native-paper';
 
 
@@ -9,24 +9,63 @@ export default function App() {
   const [botao, setBotao] = useState(true);
   const [apresentaTxt, setApresentaTxt] = useState('');
   const [carregando, setCarregando] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const validateAll = () => {
     console.log(busca);
     //setBusca(busca+"xxxx")
-    setBusca(parseInt(busca)+2)
+    setBusca(parseInt(busca)+2);
     //busca => setBusca(busca);
   }
 
-  return (
-    <View style={styles.container}>
-      {/* inicio campo busca */}
-      <Text style={styles.labelText}>Busca:</Text>
-      <TextInput style={styles.inputText} placeholder="Informe a busca" onChangeText={busca => setBusca(busca)} />{"\n"}
-      <Button title="Enviar os dados" style={styles.buttonstyle} onPress={() => {validateAll();}} color="#6200EE" />
-      <Text style={styles.labelText}>{busca}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // Inicio - Busca ou resultado da busca
+    if( carregando == false ){
+      //Nao tem retorno da API
+      if( apresentaTxt == '' ){
+        return (
+          <View style={styles.container}>
+            {/* inicio campo busca */}
+            <Text style={styles.labelText}>Busca:</Text>
+            <TextInput style={styles.inputText} placeholder="Informe a busca" onChangeText={busca => setBusca(busca)} />{"\n"}
+            <Button title="Enviar os dados" style={styles.buttonstyle} onPress={() => {validateAll();}} color="#6200EE" />
+            <Text style={styles.labelText}>{busca}</Text>
+            <StatusBar style="auto" />
+          </View>
+        );
+      }
+      else
+      {
+        //Tem retorno da API
+        return (
+          <View style={styles.container}>
+            <Text>{apresentaTxt}</Text>
+            <StatusBar style="auto" />
+          </View>
+        );        
+      }
+    // Fim - Busca ou resultado da busca
+    // Inicio - Apresenta o activity indicator  ou erro 
+    } else {
+      if(errorMessage){
+        return (
+          <View style={styles.container}>
+            <Text>{errorMessage}</Text>B
+            <StatusBar style="auto" />
+          </View>
+        );
+      } else {
+          return (
+            <View style={styles.container}>
+              <ActivityIndicator size="large" color={styles.PRIMARY_COLOR} />A
+              <StatusBar style="auto" />
+            </View>
+          );
+      }
+      // Fim - Apresenta o activity indicator  ou erro 
+    }  
+
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -41,5 +80,11 @@ const styles = StyleSheet.create({
   },  
   inputText: {
     width: 200, height: 40, backgroundColor: 'powderblue'
-  },  
+  },
+  colors: {
+    PRIMARY_COLOR: '#ff304f',
+    SECONDARY_COLOR: '#002651',
+    BORDER_COLOR: '#dbdbdb',
+  }
+  
 });
